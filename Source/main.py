4197682469c57a310119e5af7b8c3bc39b1fb10a
Jose <jgh9094@gmail.com>
2023-11-06 16:16:39 -0800

    initial push for preliminary results

---
+++ Source/main.py
@@ -0,0 +1,38 @@
+import tpot2
+import sklearn.metrics
+import sklearn
+import argparse
+import Source.utils as utils
+import sys
+
+def main():
+    # read in arguements
+    parser = argparse.ArgumentParser()
+    # number of threads to use during estimator evalutation
+    parser.add_argument("-n", "--n_jobs", default=30,  required=False, nargs='?')
+    # where to save the results/models
+    parser.add_argument("-s", "--savepath", default="binary_results", required=False, nargs='?')
+    # number of total replicates for each experiment
+    parser.add_argument("-r", "--num_reps", default=1, required=False, nargs='?')
+    # number of total replicates for each experiment
+    parser.add_argument("-s", "--sel_scheme", default=0, required=False, nargs='?')
+    schemes = ['lexicase','tournament','nsga-ii','random']
+
+    args = parser.parse_args()
+    n_jobs = int(args.n_jobs)
+    save_dir = args.savepath
+    num_reps = int(args.num_reps)
+
+    if int(args.sel_scheme) < 0 and len(schemes) <= int(args.sel_scheme):
+        sys.exit('INVALID SCHEME TO RUN')
+    scheme = [int(args.sel_scheme)]
+
+
+    # task_id_lists = [359990, 360112, 189354, 7593, 189843, 273, 359960, 189836, 75127, 168796, 167181, 75193, 168794, 189871, 189873, 189874, 189908, 189909,]
+    task_id_lists = [359990,189874,168794,189873,189871,]
+
+    utils.loop_through_tasks(scheme, task_id_lists, save_dir, num_reps, n_jobs)
+
+
+if __name__ == '__main__':
+    main()
