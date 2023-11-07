749435cdd2069d62404bdb65eb98c267aeb2a4b5
Jose <jgh9094@gmail.com>
2023-11-07 14:35:58 -0800

    fix

--- Experiments/Lexicase/HPC/msu-runner.sh
+++ Experiments/Lexicase/HPC/msu-runner.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=10
 #SBATCH -t 00:10:00
 #SBATCH --mem=100G
 #SBATCH --job-name=tpot2
@@ -12,7 +12,7 @@ conda activate tpot2-env
 pip install -e ../../../tpot2/
 
 srun -u python $HOME/Repos/GECCO-2024-TPOT2-Selection-Objectives/Source/main.py \
---n_jobs 24 \
+--n_jobs 10 \
 --savepath ../Results/ \
 --num_reps 10 \
 --sel_scheme 0 \
