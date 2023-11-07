a6b1eb2b70117ee5fe8fe182b1d10441b41c13cb
Jose <jgh9094@gmail.com>
2023-11-07 15:22:37 -0800

    fix

--- Source/utils.py
+++ Source/utils.py
@@ -54,7 +54,7 @@ def GetEstimatorParams(n_jobs, scheme):
         'mutate_then_crossover_probability': 0.0,
 
         # estimator params
-        'memory_limit':"10GB",
+        'memory_limit':"20GB",
         'preprocessing':False,
         'classification' : True,
         'verbose':5,
