89d54c8b2cbf2a72f7552146d6e0e26304e10d1f
Jose <jgh9094@gmail.com>
2023-12-04 16:50:53 -0800

    more cores

--- Experiments/Tournament/HPC/runner.sh
+++ Experiments/Tournament/HPC/runner.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=24
 #SBATCH -t 240:00:00
 #SBATCH --mem=0
 #SBATCH --job-name=jgh-tor
@@ -12,11 +12,11 @@
 
 
 source /home/hernandezj45/anaconda3/etc/profile.d/conda.sh
-conda activate tpot2-env
+conda activate tpot2-env-3.9
 pip install -e /home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/tpot2/
 
 python /home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/Source/main.py \
---n_jobs 16 \
+--n_jobs 24 \
 --savepath /home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/Experiments/Tournament/Results \
 --num_reps 10 \
 --sel_scheme 1 \
