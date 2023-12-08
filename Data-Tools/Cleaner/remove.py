9591bf5ccd3f68812f7fb53ea7feb9cf64299f18
Jose <jgh9094@gmail.com>
2023-12-07 16:44:33 -0800

    testing to see if remove failed files would work

---
+++ Data-Tools/Cleaner/remove.py
@@ -0,0 +1,66 @@
+import os
+import argparse
+import sys
+import pickle as pkl
+import shutil
+
+
+GENERATIONS = 256
+
+def SchemeSeedOffset(scheme):
+    if scheme == 0:
+        return 0
+    elif scheme == 1:
+        return 500
+    elif scheme == 2:
+        return 1000
+    else:
+        sys.exit('UTILS: INVALID SCHEME TO RUN')
+
+def main():
+    # read in arguements
+    parser = argparse.ArgumentParser()
+    # where to save the results/models
+    parser.add_argument("-d", "--data_dir", default="./", required=False, nargs='?')
+    # number of total replicates for each experiment
+    parser.add_argument("-r", "--num_reps", default=1, required=False, nargs='?')
+    # scheme we want to get data for
+    parser.add_argument("-s", "--scheme", default=0, required=False, nargs='?')
+
+    args = parser.parse_args()
+    data_dir = args.data_dir
+    num_reps = int(args.num_reps)
+    scheme = int(args.scheme)
+    seed = SchemeSeedOffset(scheme=scheme)
+
+    selection_dir = ['/Lexicase/Results/','/Tournament/Results/','/Random/Results/'][scheme]
+    print('SELECTION DIR:',selection_dir)
+    task_id_lists = [167104, 167184, 167168, 167161, 167185, 189905, 167152, 167181, 189906, 189862, 167149, 189865, 167190, 189861, 189872,
+                     168794, 189871, 168796, 168797, 75097, 126026, 189909, 126029, 126025, 75105, 168793, 189874, 167201, 189908, 189860, 168792,
+                     167083, 167200, 168798, 189873, 189866, 75127, 75193]
+
+    for task_pos, task in enumerate(task_id_lists):
+        task_limit = False
+        for rep in range(num_reps):
+            dir = data_dir + selection_dir + str(task) + '-' + str(rep + seed + (task_pos * num_reps)) + '/'
+
+            # whats the lastest folder we made it to
+            if os.path.isdir(dir) is False:
+                print('REPS:',dir)
+                task_limit = True
+                break
+
+            # check if data file exists
+            failed_pkl = dir + 'failed.pkl'
+            if os.path.exists(failed_pkl):
+                print(dir,': FAILED.PKL')
+                # shutil.rmtree(dir)
+
+
+        if task_limit:
+            break
+
+
+if __name__ == '__main__':
+    main()
+    print('FINISHED CHECKING RUNS')
---
+++ Data-Tools/Cleaner/remove.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH -t 00:00:10
+#SBATCH --mem=1GB
+#SBATCH --job-name=jgh-rmv
+#SBATCH --array=1-3
+#SBATCH -p defq,moore
+#SBATCH --exclude=esplhpc-cp040
+
+##################################
+# Setup required dependencies
+##################################
+
+source /home/hernandezj45/anaconda3/etc/profile.d/conda.sh
+conda activate tpot2-env-3.9
+
+##################################
+# Setup relevant directories
+##################################
+
+DATA_DIR=/home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/Experiments
+
+##################################
+# Setup relevant directories
+##################################
+
+SELECTION_SCHEME_LEXICASE=1
+SELECTION_SCHEME_TOURNAMENT=2
+SELECTION_SCHEME_RANDOM=3
+
+# what are we checking
+
+if [ ${SLURM_ARRAY_TASK_ID} -eq ${SELECTION_SCHEME_LEXICASE} ] ; then
+  SELECTION=0
+
+elif [ ${SLURM_ARRAY_TASK_ID} -eq ${SELECTION_SCHEME_TOURNAMENT} ] ; then
+  SELECTION=1
+
+elif [ ${SLURM_ARRAY_TASK_ID} -eq ${SELECTION_SCHEME_RANDOM} ] ; then
+  SELECTION=2
+
+else
+  echo "${SEED} from ${PROBLEM} failed to launch" >> /home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/Experiments/failtolaunch.txt
+fi
+
+# let it rip
+
+NUM_REPS=10
+
+python /home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/Data-Tools/Cleaner/remove.py \
+--data_dir ${DATA_DIR} \
+--num_reps ${NUM_REPS} \
+--scheme ${SELECTION} \
