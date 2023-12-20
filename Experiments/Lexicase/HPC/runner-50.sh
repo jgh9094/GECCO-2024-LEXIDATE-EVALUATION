c87b5886d6e9cd23a072e4da313ae78c677e80fd
Jose <jgh9094@gmail.com>
2023-12-20 12:57:47 -0800

    hpc ready

--- Experiments/Lexicase/HPC/runner-50.sh
+++ Experiments/Lexicase/HPC/runner-50.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
-#SBATCH -t 240:00:00
+#SBATCH -t 336:00:00
 #SBATCH --mem=1000GB
 #SBATCH --job-name=lex-5
 #SBATCH --exclusive
@@ -12,15 +12,14 @@
 
 source /home/hernandezj45/anaconda3/etc/profile.d/conda.sh
 conda activate tpot2-env-3.9
-pip install -e /home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/tpot2/
+pip install -e /home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/tpot2-sel-obj/
 
-DATA_DIR=/home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/Results_1/50/Lexicase
+DATA_DIR=/home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/Results_Prelim/50/Lexicase
 mkdir -p ${DATA_DIR}
 
 python /home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/Source/main-sel-obj.py \
 --n_jobs 48 \
 --savepath ${DATA_DIR} \
---num_reps 10 \
---sel_scheme 0 \
+--num_reps 30 \
 --proportion .50 \
---seed_offset 2000 \
+--seed_offset 3600 \
