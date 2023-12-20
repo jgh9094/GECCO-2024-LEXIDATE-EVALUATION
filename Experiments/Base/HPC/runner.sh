ff71b6354cc17fbecb94a906beea3d6961ff651e
Jose <jgh9094@gmail.com>
2023-12-20 10:35:59 -0800

    updates for final prelim runs

--- Experiments/Base/HPC/runner.sh
+++ Experiments/Base/HPC/runner.sh
@@ -3,9 +3,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
-#SBATCH -t 240:00:00
+#SBATCH -t 336:00:00
 #SBATCH --mem=1000GB
-#SBATCH --job-name=tpot2-b
+#SBATCH --job-name=base
 #SBATCH --exclusive
 #SBATCH -p defq,moore
 #SBATCH --exclude=esplhpc-cp040
@@ -14,11 +14,10 @@ source /home/hernandezj45/anaconda3/etc/profile.d/conda.sh
 conda activate tpot2-env-3.9
 pip install -e /home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/tpot2-base/
 
-DATA_DIR=/home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/Results_1/Base
+DATA_DIR=/home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/Results_Prelim/Base
 mkdir -p ${DATA_DIR}
 
 python /home/hernandezj45/Repos/GECCO-2024-TPOT2-Selection-Objectives/Source/main-base.py \
 --n_jobs 48 \
 --savepath ${DATA_DIR} \
---num_reps 10 \
---sel_scheme 0 \
+--num_reps 30 \
